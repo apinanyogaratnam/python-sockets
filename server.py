a128e28e077ed17701196257475160ed5b051aa7
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-29 12:14:08 -0400

    fix: handling empty message errors

--- server.py
+++ server.py
@@ -18,13 +18,14 @@ def handle_client(conn, addr):
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
-        msg_length = int(msg_length)
-        msg = conn.recv(int(msg_length.decode()))
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(int(msg_length.decode()))
 
-        if msg == DISCONNECT_MESSAGE:
-            connected = False
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
 
-        print(f"[{addr}] {msg}")
+            print(f"[{addr}] {msg}")
 
     conn.close()
 
